5119a516735d735402e997fabc2d939d06331d4b
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-02 11:12:36 +0200

    Add array columns to component List

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,17 +2,21 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 
 const List = () => {
+    const columns = [
+        { id: 1, title: 'Books', icon: 'book' },
+        { id: 2, title: 'Movies', icon: 'film' },
+        { id: 3, title: 'Games', icon: 'gamepad' },
+    ]
+
     return (
         <div className={styles.list}>
             <header className={styles.header}>Things to do <span>soon!</span></header>
             <p className={styles.description}>Interesting things I want to check out!</p>
             <section className={styles.columns}>
-                <Column title="Books" icon="book" />
-                <Column title="Movies" icon="gamepad" />
-                <Column title="Games" icon="film" />
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)}
             </section>
         </div>
     );
-  };
+};
 
-  export default List;
+export default List;

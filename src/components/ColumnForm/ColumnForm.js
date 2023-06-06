31b228e55fa0c3555778bbe74798e112ee1b45c1
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-06 11:04:08 +0200

    Use Button and TextInput in ColumnForm

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,5 +1,7 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
 
 const ColumnForm = props => {
     const [title, setTitle] = useState('');
@@ -14,10 +16,10 @@ const ColumnForm = props => {
     return (
         <form onSubmit={handleSubmit}>
             <span>Title: </span>
-            <input type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <TextInput value={title} onChange={e => setTitle(e.target.value)} />
             <span>Icon: </span>
-            <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
-            <button>Add column</button>
+            <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
+            <Button>Add column</Button>
         </form>
     );
 }

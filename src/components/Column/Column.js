a4a72a9c6552e9ae2a7dc7c23db0e8d42e699a6a
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-07 08:23:52 +0200

    Add card form for each column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,13 +1,27 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
+import { useState } from 'react';
+import shortid from 'shortid';
+import { useEffect } from 'react';
 
 const Column = props => {
+    const [cards, setCards] = useState([...props.cards]);
+    const addCard = newCard => {
+        setCards([...cards, { id: shortid(), title: newCard.title }]);
+    }
+
+    useEffect(() => {
+      console.log('Cards:', cards);
+    }, [cards])
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon}></span>{props.title}</h2>
             <ul className={styles.cards}>
                 {props.cards.map(card => <Card key={card.id}>{card.title}</Card>)}
             </ul>
+            <CardForm action={addCard} />
         </article>
     );
 }

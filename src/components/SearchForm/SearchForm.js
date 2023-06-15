407fd8b4855216a8a9561e6825442daeb36c3402
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-15 17:33:24 +0200

    Search for cards which includes phrase in searchbar

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,20 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const [phrase, setPhrase] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch({ type: 'CHANGE_PHRASE', payload: phrase });
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search..." value={phrase} onChange={e => setPhrase(e.target.value)} />
             <Button>
                 <span className="fa fa-search" />
             </Button>

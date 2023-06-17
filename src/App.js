e627d8602c585203cce0f5b02aa2a6c85bcd7ee2
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-17 21:49:07 +0200

    Create an error page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import Navbar from './components/NavBar/NavBar.js';
 import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Favorite from './components/Favorite/Favorite.js';
+import ErrorPage from './components/ErrorPage/ErrorPage.js';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Container>
-        <Home />
-        <About />
-        <Favorite />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       </Container>
     </main>
   );
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,10 @@
+import styles from './ErrorPage.module.scss';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = () => {
+    return (
+        <PageTitle>404 not found</PageTitle>
+    );
+  };
+  
+  export default Favorite;

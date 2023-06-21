aefe4b2ae86bd6e79f3348fe92f9d19633e2640d
Kevin Kaluzny <kevin.evangelo.k@gmail>
2023-06-21 12:40:53 +0200

    Unfinished work

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,6 +17,7 @@ export const getAllLists = ({ lists }) => lists;
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 export const addCard = payload => ({ type: 'ADD_CARD', payload });
 export const addList = payload => ({ type: 'ADD_LIST', payload });
+export const toggleCardFavorite = payload => ( {type: 'TOGGLE_CARD_FAVORITE', payload });
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -28,6 +29,8 @@ const reducer = (state, action) => {
       return { ...state, searchString: action.payload };
     case 'ADD_LIST':
       return { ...state, lists: [...state.lists, { id: shortid(), ...action.payload }] };
+    case 'TOGGLE_CARD_FAVORITE':
+      return { ...state, cards: state.cards.map(card => (card.id === action.payload) ? { ...card, isFavorite: !card.isFavorite } : card) };
   }
   return state;
 };
